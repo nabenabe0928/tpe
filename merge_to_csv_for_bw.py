8001829b66ac8a13ba3a4cb675f275bac1058ad8
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2023-02-20 13:07:29 +0100

    Add additional analysis

---
+++ merge_to_csv_for_bw.py
@@ -0,0 +1,74 @@
+import os
+
+import numpy as np
+
+import pandas as pd
+
+import ujson as json
+
+
+if __name__ == "__main__":
+    dir_names = [d for d in os.listdir("results-bandwidth/") if not d.endswith(".csv") and "recommended" not in d]
+    data = {
+        "multivariate": [],
+        "quantile": [],
+        "alpha": [],
+        "weight": [],
+        "magic-clip": [],
+        "heuristic": [],
+        "min_bandwidth_factor": [],
+    }
+    epochs = [epoch - 1 for epoch in [50, 100, 150, 200]]
+    mean_vals = {
+        "setting_index": [],
+        "target": [],
+    }
+    mean_vals.update({f"n_evals{i+1:0>3}": [] for i in range(200)})
+    results = {
+        "setting_index": [],
+        "target": [],
+        "mean@n_evals050": [],
+        "mean@n_evals100": [],
+        "mean@n_evals150": [],
+        "mean@n_evals200": [],
+        "ste@n_evals050": [],
+        "ste@n_evals100": [],
+        "ste@n_evals150": [],
+        "ste@n_evals200": [],
+    }
+    cols = list(data.keys())
+
+    index = 0
+    for dir_name in dir_names:
+        for col in cols:
+            if (col + "=") not in dir_name:
+                data[col].append(None)
+                continue
+
+            loc = dir_name.find(col)
+            val = dir_name[loc:].split("=")[1].split("_")[0]
+            data[col].append(val)
+        else:
+            dir_path = os.path.join("results-bandwidth", dir_name)
+            files = os.listdir(dir_path)
+            for fn in files:
+                cum_vals = np.minimum.accumulate(json.load(open(os.path.join(dir_path, fn))), axis=-1)
+                means = np.mean(cum_vals, axis=0)
+                stes = np.std(cum_vals[:, epochs], axis=0) / np.sqrt(cum_vals.shape[0])
+                results["setting_index"].append(index)
+                results["target"].append(fn.split(".json")[0])
+                mean_vals["setting_index"].append(index)
+                mean_vals["target"].append(fn.split(".json")[0])
+                for i in range(4):
+                    e = 50 * (i + 1)
+                    results[f"mean@n_evals{e:0>3}"].append(means[epochs[i]])
+                    results[f"ste@n_evals{e:0>3}"].append(stes[i])
+                for i in range(200):
+                    mean_vals[f"n_evals{i+1:0>3}"].append(means[i])
+
+        index += 1
+
+    pd.DataFrame(data).to_csv("results-bandwidth/setting-table.csv")
+    pd.DataFrame(results).to_csv("results-bandwidth/summary.csv", index=False)
+    mean_vals = {k: np.asarray(v, dtype=np.float32) if k.startswith("n_evals") else v for k, v in mean_vals.items()}
+    pd.DataFrame(mean_vals).to_csv("results-bandwidth/mean_vals.csv", index=False)
