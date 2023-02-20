320bdb7c89b36292e179e8087b59baff8c8ce1eb
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2023-02-20 14:08:07 +0100

    Add the random script

---
+++ scripts/run_random.py
@@ -0,0 +1,90 @@
+import json
+import os
+from abc import ABCMeta
+from typing import Callable, Optional
+
+import ConfigSpace as CS
+
+from tpe.optimizer import TPEOptimizer
+from tpe.utils.benchmarks import (
+    Sphere,
+    Styblinski,
+    Rastrigin,
+    Schwefel,
+    Ackley,
+    Griewank,
+    Perm,
+    KTablet,
+    WeightedSphere,
+    Rosenbrock,
+    Levy,
+    XinSheYang,
+)
+from tpe.utils.tabular_benchmarks import HPOBench, HPOLib, JAHSBench201
+
+
+FUNCS = [
+    Sphere,
+    Styblinski,
+    Rastrigin,
+    Schwefel,
+    Ackley,
+    Griewank,
+    Perm,
+    KTablet,
+    WeightedSphere,
+    Rosenbrock,
+    Levy,
+    XinSheYang,
+]
+FUNCS += [HPOBench(dataset_id=i, seed=None) for i in range(8)]
+FUNCS += [HPOLib(dataset_id=i, seed=None) for i in range(4)]
+FUNCS += [JAHSBench201(dataset_id=i) for i in range(3)]
+
+
+def collect_data(bench: Callable, dim: Optional[int] = None) -> None:
+    bench_name = bench().__class__.__name__ if isinstance(bench, ABCMeta) else bench.dataset_name
+    bench_name = f"{bench_name}_{dim:0>2}d" if dim is not None else bench_name
+    print(bench_name, dim)
+
+    config_space = CS.ConfigurationSpace()
+    if dim is not None:
+        config_space.add_hyperparameters(
+            [CS.UniformFloatHyperparameter(f"x{d}", lower=-1, upper=1) for d in range(dim)]
+        )
+
+    dir_name = "results/random"
+    os.makedirs(dir_name, exist_ok=True)
+    path = os.path.join(dir_name, f"{bench_name}.json")
+    if os.path.exists(path):
+        print(f"{path} exists; skip")
+        return
+
+    results = []
+    for seed in range(10):
+        if hasattr(bench, "reseed"):
+            bench.reseed(seed)
+
+        config_space = config_space if isinstance(bench, ABCMeta) else bench.config_space
+        random_sampler = TPEOptimizer(
+            obj_func=bench.func if isinstance(bench, ABCMeta) else bench,
+            config_space=config_space,
+            n_init=200,
+            max_evals=200,
+            seed=seed,
+        )
+        random_sampler.optimize()
+        data = random_sampler.fetch_observations()
+        results.append(data["loss"].tolist())
+    else:
+        with open(path, mode="w") as f:
+            json.dump(results, f, indent=4)
+
+
+if __name__ == "__main__":
+    for bench in FUNCS:
+        if isinstance(bench, ABCMeta):
+            for d in [5, 10, 30]:
+                collect_data(bench, dim=d)
+        else:
+            collect_data(bench)
