66b8980b23e59a1f2a590d5b4f23418c27a67dc4
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2023-04-11 20:45:09 +0200

    Add YAHPO and change the output of each objective

--- tpe/utils/tabular_benchmarks.py
+++ tpe/utils/tabular_benchmarks.py
@@ -1,7 +1,7 @@
 import json
 import os
 from abc import ABCMeta, abstractmethod
-from typing import Any, Dict, Final, List, Optional, Union
+from typing import Dict, Final, List, Optional, Union
 
 import ConfigSpace as CS
 
@@ -14,12 +14,14 @@ except ModuleNotFoundError:  # We cannot use jahs with smac
 
 import numpy as np
 
-import pyarrow.parquet as pq  # type: ignore
+from yahpo_gym import benchmark_set, local_config
 
 
 DATA_DIR_NAME = os.path.join(os.environ["HOME"], "tabular_benchmarks")
 SEEDS: Final = [665, 1319, 7222, 7541, 8916]
 VALUE_RANGES = json.load(open("tpe/utils/tabular_benchmarks.json"))
+local_config.init_config()
+local_config.set_data_path(DATA_DIR_NAME)
 
 
 class AbstractBench(metaclass=ABCMeta):
@@ -47,66 +49,95 @@ class AbstractBench(metaclass=ABCMeta):
     def config_space(self) -> CS.ConfigurationSpace:
         raise NotImplementedError
 
+    @property
+    @abstractmethod
+    def max_budget(self) -> int:
+        raise NotImplementedError
 
-class HPOBench(AbstractBench):
+
+class LCBench(AbstractBench):
+    # https://syncandshare.lrz.de/getlink/fiCMkzqj1bv1LfCUyvZKmLvd/
+    _target_metric = "val_balanced_accuracy"
+
     def __init__(
         self,
         dataset_id: int,
-        seed: Optional[int],
+        seed: Optional[int] = None,  # surrogate is not stochastic
     ):
-        # https://ndownloader.figshare.com/files/30379005
-        dataset_info = [
-            ("credit_g", 31),
-            ("vehicle", 53),
-            ("kc1", 3917),
-            ("phoneme", 9952),
-            ("blood_transfusion", 10101),
-            ("australian", 146818),
-            ("car", 146821),
-            ("segment", 146822),
-        ]
-        self.dataset_name, dataset_id = dataset_info[dataset_id]
-        budget_name = "iter"
-        data_path = os.path.join(DATA_DIR_NAME, "hpo-bench", str(dataset_id), f"nn_{dataset_id}_data.parquet.gzip")
-        db = pq.read_table(data_path, filters=[(budget_name, "==", 243)])
-        self._db = db.drop([budget_name, "subsample"])
-        self._rng = np.random.RandomState(seed)
-        self._value_range = VALUE_RANGES["hpo-bench"]
-        self._cache: Dict[int, float] = {}
+        dataset_info = (
+            ("kddcup09_appetency", "3945"),
+            ("covertype", "7593"),
+            ("amazon_employee_access", "34539"),
+            ("adult", "126025"),
+            ("nomao", "126026"),
+            ("bank_marketing", "126029"),
+            ("shuttle", "146212"),
+            ("australian", "167104"),
+            ("kr_vs_kp", "167149"),
+            ("mfeat_factors", "167152"),
+            ("credit_g", "167161"),
+            ("vehicle", "167168"),
+            ("kc1", "167181"),
+            ("blood_transfusion_service_center", "167184"),
+            ("cnae_9", "167185"),
+            ("phoneme", "167190"),
+            ("higgs", "167200"),
+            ("connect_4", "167201"),
+            ("helena", "168329"),
+            ("jannis", "168330"),
+            ("volkert", "168331"),
+            ("mini_boo_ne", "168335"),
+            ("aps_failure", "168868"),
+            ("christine", "168908"),
+            ("fabert", "168910"),
+            ("airlines", "189354"),
+            ("jasmine", "189862"),
+            ("sylvine", "189865"),
+            ("albert", "189866"),
+            ("dionis", "189873"),
+            ("car", "189905"),
+            ("segment", "189906"),
+            ("fashion_mnist", "189908"),
+            ("jungle_chess_2pcs_raw_endgame_complete", "189909")
+        )
+        self.dataset_name, self._dataset_id = dataset_info[dataset_id]
+        self._surrogate = benchmark_set.BenchmarkSet("lcbench", instance=self._dataset_id)
+        self._config_space = self.config_space
 
-    def _validate_query(self, query: Dict[str, Any], config: Dict[str, Union[int, float]]) -> None:
-        if len(query["__index_level_0__"]) != 1:
-            raise ValueError(f"There must be only one row for config={config}, but got query={query}")
+    def __call__(self, config: Dict[str, Union[int, float]], budget: int = 52) -> Dict[str, float]:
+        EPS = 1e-12
+        for hp in self._config_space.get_hyperparameters():
+            lb, ub, name = hp.lower, hp.upper, hp.name
+            if isinstance(hp, CS.UniformFloatHyperparameter):
+                assert isinstance(config[name], float) and lb - EPS <= config[name] <= ub + EPS
+            else:
+                config[name] = int(config[name])
+                assert isinstance(config[name], int) and lb <= config[name] <= ub
 
-        queried_config = {k: query[k][0] for k in config.keys()}
-        if not all(np.isclose(queried_config[k], v, rtol=1e-3) for k, v in config.items()):
-            raise ValueError(f"The query must have the identical config as {config}, but got {queried_config}")
-
-    def __call__(self, config: Dict[str, int]) -> float:
-        _config = config.copy()
-        _config["seed"] = SEEDS[self._rng.randint(len(SEEDS))]
-        KEY_ORDER = ["alpha", "batch_size", "depth", "learning_rate_init", "width", "seed"]
-
-        assert len(_config) == len(KEY_ORDER)
-        idx = 0
-        eval_config: Dict[str, Union[int, float]] = {}
-        for k in KEY_ORDER:
-            true_val = self._value_range[k][_config[k]] if k != "seed" else _config[k]
-            assert not isinstance(true_val, str)  # mypy redefinition
-            eval_config[k] = true_val
-            idx = self._db[k].index(true_val, start=idx).as_py()
-
-        if idx in self._cache:
-            return self._cache[idx]
-
-        query = self._db.take([idx]).to_pydict()
-        self._validate_query(query, eval_config)
-        self._cache[idx] = 1.0 - query["result"][0]["info"]["val_scores"]["bal_acc"]
-        return self._cache[idx]
+        config["OpenML_task_id"] = self._dataset_id
+        config["epoch"] = budget
+        output = self._surrogate.objective_function(config)[0]
+        return dict(loss=1.0 - output[self._target_metric], runtime=output["time"])
 
     @property
     def config_space(self) -> CS.ConfigurationSpace:
-        return self._fetch_discrete_config_space()
+        config_space = CS.ConfigurationSpace()
+        config_space.add_hyperparameters(
+            [
+                CS.UniformIntegerHyperparameter(name="batch_size", lower=16, upper=512, log=True),
+                CS.UniformFloatHyperparameter(name="learning_rate", lower=1e-4, upper=0.1, log=True),
+                CS.UniformFloatHyperparameter(name="max_dropout", lower=0.0, upper=1.0),
+                CS.UniformIntegerHyperparameter(name="max_units", lower=64, upper=1024, log=True),
+                CS.UniformFloatHyperparameter(name="momentum", lower=0.1, upper=0.9),
+                CS.UniformIntegerHyperparameter(name="num_layers", lower=1, upper=5),
+                CS.UniformFloatHyperparameter(name="weight_decay", lower=1e-5, upper=0.1),
+            ]
+        )
+        return config_space
+
+    @property
+    def max_budget(self) -> int:
+        return 52
 
 
 class HPOLib(AbstractBench):
@@ -132,14 +163,20 @@ class HPOLib(AbstractBench):
         self._rng = np.random.RandomState(seed)
         self._value_range = VALUE_RANGES["hpolib"]
 
-    def __call__(self, config: Dict[str, Union[int, str]]) -> float:
+    def __call__(self, config: Dict[str, Union[int, str]], budget: int = 100) -> Dict[str, float]:
         idx = self._rng.randint(4)
         key = json.dumps({k: self._value_range[k][int(v)] for k, v in config.items()}, sort_keys=True)
-        return np.log(self._db[key]["valid_mse"][idx][99])
+        loss = self._db[key]["valid_mse"][idx][budget - 1]
+        runtime = self._db[key]["runtime"][idx] * budget / self.max_budget
+        return dict(loss=np.log(loss), runtime=runtime)
 
     @property
     def config_space(self) -> CS.ConfigurationSpace:
         return self._fetch_discrete_config_space()
+
+    @property
+    def max_budget(self) -> int:
+        return 100
 
 
 class JAHSBench201(AbstractBench):
@@ -155,18 +192,19 @@ class JAHSBench201(AbstractBench):
         self.dataset_name = ["cifar10", "fashion_mnist", "colorectal_histology"][dataset_id]
         data_dir = os.path.join(DATA_DIR_NAME, "jahs_bench_data")
         self._surrogate = jahs_bench.Benchmark(
-            task=self.dataset_name, download=False, save_dir=data_dir, metrics=[self._target_metric]
+            task=self.dataset_name, download=False, save_dir=data_dir, metrics=[self._target_metric, "runtime"]
         )
         self._value_range = VALUE_RANGES["jahs-bench"]
 
-    def __call__(self, config: Dict[str, Union[int, str, float]]) -> float:
+    def __call__(self, config: Dict[str, Union[int, str, float]], budget: int = 200) -> Dict[str, float]:
         EPS = 1e-12
         config = {k: self._value_range[k][int(v)] if k in self._value_range else float(v) for k, v in config.items()}
         assert isinstance(config["LearningRate"], float) and 1e-3 - EPS <= config["LearningRate"] <= 1.0 + EPS
         assert isinstance(config["WeightDecay"], float) and 1e-5 - EPS <= config["WeightDecay"] <= 1e-2 + EPS
         config.update(Optimizer="SGD", Resolution=1.0)
         config = {k: int(v) if k[:-1] == "Op" else v for k, v in config.items()}
-        return 100 - self._surrogate(config, nepochs=200)[200][self._target_metric]
+        output = self._surrogate(config, nepochs=budget)[budget]
+        return dict(loss=100 - output[self._target_metric], runtime=output["runtime"])
 
     @property
     def config_space(self) -> CS.ConfigurationSpace:
@@ -178,3 +216,7 @@ class JAHSBench201(AbstractBench):
             ]
         )
         return config_space
+
+    @property
+    def max_budget(self) -> int:
+        return 200
