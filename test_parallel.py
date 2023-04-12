a3d3de4ea6302c57cdb0df83fcfc4f043c447e31
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2023-04-12 22:30:23 +0200

    Finish the timestamp feat

--- test_parallel.py
+++ test_parallel.py
@@ -2,7 +2,9 @@ import glob
 import hashlib
 import os
 import time
-from typing import Callable, Dict
+from typing import Any, Callable, Dict, Protocol
+
+import numpy as np
 
 import ujson as json
 
@@ -20,8 +22,9 @@ def generate_time_hash() -> str:
     return hash.hexdigest()
 
 
-def publish_token(public_token: str, private_token: str, waiting_time: float = 1e-5) -> None:
+def publish_token(public_token: str, private_token: str, waiting_time: float = 1e-4) -> None:
     start = time.time()
+    waiting_time *= 1 + np.random.random()
     while True:
         try:
             os.rename(public_token, private_token)
@@ -106,19 +109,24 @@ def is_min_cumtime(public_token: str, private_token: str, proc_id: str, dir_name
 
 
 def wait_all_procs(
-    public_token: str, private_token: str, n_procs: int, dir_name: str, waiting_time: float = 1e-5
+    public_token: str, private_token: str, n_procs: int, dir_name: str, waiting_time: float = 5e-3
 ) -> None:
+    start = time.time()
     kwargs = dict(public_token=public_token, private_token=private_token, n_procs=n_procs, dir_name=dir_name)
     while True:
+        waiting_time *= 1.02
         if is_scheduler_ready(**kwargs):
             return
         else:
             time.sleep(waiting_time)
+            if time.time() - start >= 5:
+                raise TimeoutError("Timeout in creating a scheduler. Please make sure n_procs is correct.")
 
 
 def wait_until_next(
-    public_token: str, private_token: str, proc_id: str, dir_name: str, waiting_time: float = 1e-5
+    public_token: str, private_token: str, proc_id: str, dir_name: str, waiting_time: float = 1e-4
 ) -> None:
+    waiting_time *= 1 + np.random.random()
     kwargs = dict(public_token=public_token, private_token=private_token, proc_id=proc_id, dir_name=dir_name)
     while True:
         if is_min_cumtime(**kwargs):
@@ -127,9 +135,41 @@ def wait_until_next(
             time.sleep(waiting_time)
 
 
-def get_wrapper_func(
-    public_token: str, private_token: str, proc_id: str, wrapper_func: Callable, n_procs: int, dir_name: str
-) -> Callable:
+class ObjectiveFunc(Protocol):
+    def __call__(self, eval_config: Dict[str, Any]) -> Dict[str, float]:
+        raise NotImplementedError
+
+
+class WrapperFunc:
+    def __init__(self, public_token: str, private_token: str, proc_id: str, dir_name: str, func: ObjectiveFunc):
+        self._kwargs = dict(public_token=public_token, private_token=private_token, proc_id=proc_id, dir_name=dir_name)
+        record_runtime(**self._kwargs, runtime=0.0)
+        self._func = func
+        self._proc_id = proc_id
+        self._prev_timestamp = time.time()
+
+    def __call__(self, eval_config: Dict[str, Any]) -> Dict[str, float]:
+        sampling_time = time.time() - self._prev_timestamp
+        output = self._func(eval_config)
+        loss, runtime = output["loss"], output["runtime"]
+        cumtime = record_runtime(**self._kwargs, runtime=runtime+sampling_time)
+        wait_until_next(**self._kwargs)
+        self._prev_timestamp = time.time()
+        row = dict(loss=loss, cumtime=cumtime, proc_id=self._proc_id, timestamp=self._prev_timestamp)
+        record_result(**self._kwargs, results=row)
+        return output
+
+    def finish(self) -> None:
+        inf_time = 1 << 40
+        record_runtime(**self._kwargs, runtime=inf_time)
+
+
+def get_wrapper_func(func: ObjectiveFunc, n_procs: int, subdir_name: str) -> WrapperFunc:
+    proc_id = generate_time_hash()
+    dir_name = os.path.join(DIR_NAME, subdir_name)
+    public_token = os.path.join(dir_name, PUBLIC_TOKEN_NAME)
+    private_token = os.path.join(dir_name, f"scheduler_{proc_id}.token")
+
     os.makedirs(dir_name, exist_ok=True)
     token_pattern = os.path.join(dir_name, TOKEN_PATTERN)
     scheduler_path = os.path.join(dir_name, SCHEDULER_FILE_NAME)
@@ -137,40 +177,18 @@ def get_wrapper_func(
     init_token(token_pattern=token_pattern, public_token=public_token)
     kwargs = dict(public_token=public_token, private_token=private_token)
     init_scheduler(**kwargs, scheduler_path=scheduler_path, result_path=result_path)
-    func = wrapper_func(**kwargs, proc_id=proc_id, dir_name=dir_name)
+    _func = WrapperFunc(**kwargs, func=func, proc_id=proc_id, dir_name=dir_name)
     wait_all_procs(**kwargs, n_procs=n_procs, dir_name=dir_name)
-    return func
-
-
-def wrapper_func(public_token: str, private_token: str, proc_id: str, dir_name: str) -> Callable:
-    kwargs = dict(public_token=public_token, private_token=private_token, proc_id=proc_id, dir_name=dir_name)
-    record_runtime(**kwargs, runtime=0.0)
-
-    def _func(x: float, sampling_time: float) -> Dict[str, float]:
-        start = time.time()
-        loss_val = x ** 2
-        runtime = time.time() - start
-        cumtime = record_runtime(**kwargs, runtime=runtime+sampling_time)
-        wait_until_next(**kwargs)
-        record_result(**kwargs, results=dict(loss=loss_val, cumtime=cumtime, proc_id=proc_id))
-        return dict(loss=loss_val, runtime=runtime)
-
     return _func
 
 
 if __name__ == "__main__":
-    proc_id = generate_time_hash()
-    dir_name = os.path.join(DIR_NAME, "test")
-    kwargs = dict(
-        proc_id=proc_id,
-        dir_name=dir_name,
-        public_token=os.path.join(dir_name, PUBLIC_TOKEN_NAME),
-        private_token=os.path.join(dir_name, f"scheduler_{proc_id}.token"),
+    func = get_wrapper_func(
+        func=lambda eval_config: dict(loss=eval_config["x"] ** 2, runtime=1.0),
+        n_procs=4,
+        subdir_name="test"
     )
-    func = get_wrapper_func(wrapper_func=wrapper_func, n_procs=4, **kwargs)
-
     for i in range(10):
-        func(0.1 * i, 0)
+        func({"x": 0.1 * i})
     else:
-        inf_time = 1 << 40
-        record_runtime(**kwargs, runtime=inf_time)
+        func.finish()
