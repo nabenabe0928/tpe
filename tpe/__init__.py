9a7473d611a4ad5c29a9c62de293596729ca7b3f
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-03-09 06:07:24 +0100

    Add future annotations to all the files

--- tpe/__init__.py
+++ tpe/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from tpe import optimizer
 from tpe.utils import utils
 
--- tpe/optimizer/__init__.py
+++ tpe/optimizer/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import warnings
 
 from tpe.optimizer.random_search import RandomSearch
